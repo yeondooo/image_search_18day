074d4857de6a356f6fa10914517eda3156491fc6
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-08-06 22:17:12 +0900

    feat: main view model 구현

---
+++ lib/presentation/main/main_view_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:image_search_18day/core/result.dart';
+import 'package:image_search_18day/domain/use_case/get_top_five_viewd_photo_use_case.dart';
+import 'package:image_search_18day/presentation/main/main_state.dart';
+
+class MainViewModel with ChangeNotifier {
+  final GetTopFiveViewdPhotoUseCase _getTopFiveViewdPhotoUseCase;
+
+  MainViewModel(this._getTopFiveViewdPhotoUseCase);
+
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  void fetchImages(String query) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+    final result = await _getTopFiveViewdPhotoUseCase.execute(query);
+
+    switch (result) {
+      case Success(:final data):
+        _state = state.copyWith(
+          photos: data,
+          isLoading: false,
+        );
+      case Error(:final e):
+        _state = state.copyWith(
+          isLoading: false,
+        );
+    }
+    notifyListeners();
+  }
+}
